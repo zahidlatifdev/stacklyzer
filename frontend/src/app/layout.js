9aac212a3e8d705dee13e9904090702d83c2dac9
Zahid Latif <muhammadzahidlatif123@gmail.com>
2025-04-14 00:09:33 +0500

    Add @vercel/analytics integration and update package.json and package-lock.json

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 import { ThemeProvider } from "./context/ThemeProvider";
 
@@ -27,6 +28,7 @@ export default function RootLayout({ children }) {
         <ThemeProvider>
           {children}
           <SpeedInsights />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
